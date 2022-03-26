c700cfee609145545ad0ad24437e05d869bc0250
Marcos Soares <mavmaso@gmail.com>
2022-03-26 11:47:37 -0300

    add home w/ tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[get("/")]
+async fn greet() -> impl Responder {
+    format!("Hello Mundo!")
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new()
-        .route("/health_check", web::get().to(health_check)))
+        .route("/health_check", web::get().to(health_check))
+        .service(greet)
+        )
         .listen(listener)?
         .run();
 
